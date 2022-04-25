f47bf60cd7f81991947f6cd14d58cbf6569b5923
tutao <542366504@qq.com>
2022-04-25 23:51:08 +0800

    ORM

---
+++ student/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Student(models.Model):
+    SEX_CHOICES = (
+        (0, "女"),
+        (1, "男"),
+        (2, "保密"),
+    )
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=32, unique=True, verbose_name="姓名")
+    age = models.SmallIntegerField(default=18, verbose_name="年龄")
+    sex = models.SmallIntegerField(choices=SEX_CHOICES)
+    birthday = models.DateField()
+
+    classmate = models.CharField(db_column="class", max_length=5, db_index=True, verbose_name="班级", default="")
+    description = models.TextField(default="", verbose_name="个性签名")
+    created_time = models.DateTimeField(default="%Y-%m-%d %H:%M:%S")
+    updated_time = models.DateTimeField(default="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        db_table = "db_student"
